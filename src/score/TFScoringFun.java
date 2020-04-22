e646ec4b8730c064e5fbb94bd3bdc98c62861003
Foong-Ann <54426916+Foong-Ann@users.noreply.github.com>
2020-04-22 21:28:33 +0800

    Update TFScoringFun.java

--- src/score/TFScoringFun.java
+++ src/score/TFScoringFun.java
@@ -6,13 +6,11 @@ public class TFScoringFun implements TermScoringFun {
 
 	@Override
 	public void init(Index s) {
-		// Do nothing here for TF... no need to compute IDF so no need to keep track of the Index.
-		// Note that for TFIDF, the Index gives you access to the document frequency of any term!
 	}
 
 	@Override
 	public double scoreToken(String term, int freq) {
-		return (double)freq; // Notes: (1) This is a non-log TF; (2) Ignore term since IDF irrelevant
+		return (double)freq; 
 	}
 
 }
